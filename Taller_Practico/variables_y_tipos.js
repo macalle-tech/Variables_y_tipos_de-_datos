4f02b865feb7e24a2439714bf61f18ca775e4b10
Mario Andres Calle <ma.calle@unitecnica.net>
2026-02-20 00:13:04 -0500

    Taller practico variables y tipos de datos

---
+++ Taller_Practico/variables_y_tipos.js
@@ -0,0 +1,57 @@
+let nombre = "Mario Andres Calle"
+let edad = 39
+const pais = "Colombia"
+
+console.log(nombre)
+console.log(edad)
+console.log(pais)
+
+
+
+let decimal = 3.14
+let mensaje = "Hola estoy aprendiendo Javascript"
+let verdadero = true
+let sin_valor = undefined
+let vacio = null
+
+console.log(decimal)
+console.log(mensaje)
+console.log(verdadero)
+console.log(sin_valor)
+console.log(vacio)
+
+
+console.log(typeof decimal)
+console.log(typeof mensaje)
+console.log(typeof verdadero)
+console.log(typeof sin_valor)
+console.log(typeof vacio)
+
+
+let precio = 5000
+let cantidad = 3
+let total = precio * cantidad
+
+console.log("El Total a pagar es: $"+total)
+
+
+let persona = {
+    nombre: "Mario Andres Calle",
+    edad: 39,
+    ciudad: "Manizales"
+}
+
+console.log(persona.nombre)
+console.log(persona.edad)
+console.log(persona.ciudad)
+
+
+let estudiante = "Andres Calle"
+let nota1 = 85
+let nota2 = 90
+let nota3 = 78
+
+let promedio = (nota1 + nota2 + nota3) / 3
+
+console.log("El estudiante " + estudiante + " tiene promedio " + promedio)
+
